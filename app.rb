3034c84671f0b55ffe87305e371d6a7a472f55ca
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-18 03:21:10 -0800

    updated

--- app.rb
+++ app.rb
@@ -4,15 +4,77 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, 'sqlite3:barbershop.db'
+set :database, {adapter:'sqlite3', database:'barbershop.db'}
  
-class Client < ActiveRecord::Base
+class User < ActiveRecord::Base
 end
 
 class Barber < ActiveRecord::Base
 end
+before do
+	@barbers = Barber.all
+	@results = User.all
+end
 
 get '/' do
-	@barbers = Barber.all
+	# @barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/showusers' do
+ 	erb :showusers
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время',
+			:barber => 'Выберите Парекмахера',
+			:color => 'Выберите цвет покраски волос'
+		}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	# $db.execute 'insert into Users 
+	# (
+	# 	username, 
+	# 	phone, 
+	# 	datestamp,
+	# 	barber, 
+	# 	color
+	# )
+	# values
+	# (
+	# ?, ?, ?, ?, ?
+	# )', [@username, @phone, @datetime, @barber, @color]
+
+	erb "<h3>Спасибо, вы записались!</h3>"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_045959) do
+ActiveRecord::Schema.define(version: 2021_02_18_103425) do
 
   create_table "barbers", force: :cascade do |t|
     t.text "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_045959) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "clients", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
